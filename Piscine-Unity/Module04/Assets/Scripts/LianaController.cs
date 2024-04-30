e143cf33b944e25f1dd77dc458414a9e629e1cc2
Fartomy <furkii_tekdemir@hotmail.com>
2024-04-30 16:42:26 +0300

    module04 completed

--- Piscine-Unity/Module04/Assets/Scripts/LianaController.cs
+++ Piscine-Unity/Module04/Assets/Scripts/LianaController.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class LianaController : MonoBehaviour
 {
+    [SerializeField] private AudioClip _lianaHitClip;
     private Animator _animator;
+    private AudioManager _audioManager;
 
     void Awake()
     {
+        _audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
         _animator = GetComponent<Animator>();
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
-            _animator.SetTrigger("Attack");
+            InvokeRepeating("LianaAttack", 0, 1);
+    }
+
+    void LianaAttack()
+    {
+        _animator.SetTrigger("Attack");
+        _audioManager.PlayAudioClip(_lianaHitClip, transform, 1);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        CancelInvoke("LianaAttack");
     }
 }
